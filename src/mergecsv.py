bd598ecf0575d75cbb8b068de06705f5981a6e8d
Tris Emmy Wilson <tris@tris.fyi>
2022-10-08 16:53:07 -0500

    merged data

--- src/mergecsv.py
+++ src/mergecsv.py
@@ -4,7 +4,13 @@ import sys
 data = []
 header = None
 
+def spnorm(s):
+    return " ".join(s.split())
+
 for i in sys.argv[1:-1]:
+    if i == sys.argv[-1]:
+        continue
+
     with open(i) as f:
         r = csv.reader(f)
         for row in r:
@@ -14,6 +20,16 @@ for i in sys.argv[1:-1]:
             if row[0] == "fund":
                 header = row
                 continue
+
+            fund, account, subaccount, long_account, vendor, invoice, line_item_desc, check, date, amount = row
+
+            vendor = spnorm(vendor)
+            invoice = spnorm(invoice)
+            line_item_desc = spnorm(line_item_desc)
+            if check.endswith(".0"):
+                check = check[:-2]
+
+            row = fund, account, subaccount, long_account, vendor, invoice, line_item_desc, check, date, amount
 
             data.append(row)
 
